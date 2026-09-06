6fd710191bd1041ca271e866c6717c1fbeb1eefa
unknown <jerry.wan@mail.utoronto.ca>
2026-09-05 21:22:44 -0400

    Experimenting with some preprocessing options from open cv

--- image_processing.py
+++ image_processing.py
@@ -11,3 +11,20 @@ def show_image(image: np.array):
     cv2.imshow("Window capture", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def crop_image(left, right, top, bottom, image):
+    return image[top:bottom, left:right]
+
+def colour_mask(lowerbound, upperbound, image):
+    return cv2.inRange(image, np.array(lowerbound), np.array(upperbound))
+
+def get_contours(image):
+    ret, thresh = cv2.threshold(image, 60, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+    copy = image.copy()
+    cv2.drawContours(image=copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                     lineType=cv2.LINE_AA)
+    return copy
+
+def denoise(image):
+    return cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
--- main.py
+++ main.py
@@ -13,6 +13,11 @@ if __name__ == '__main__':
         bitmap = cw.capture_window(terraria_window[0])
         image = imgproc.strip_alpha(bitmap)
         imgproc.save_image('latest_capture.png', image)
-        imgproc.show_image(bitmap)
+        cellphone_info_img = imgproc.crop_image(1560, 1860, 440, 780, image)
+        # masked_image = imgproc.colour_mask([65,65,65],[255,255,255], cellphone_info_img)
+        # contour_image = imgproc.get_contours(masked_image)
+        denoised_image = imgproc.denoise(cellphone_info_img)
+        imgproc.show_image(cellphone_info_img)
+        imgproc.show_image(denoised_image)
     else:
         print("Error: Found " + str(len(matching_windows)) + " windows matching title pattern")
